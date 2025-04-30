a998b47cdd12dd7da6a98008c6620792dfdde99f
Tasigen <hibiscus_ts@yahoo.com>
2025-05-01 00:49:54 +0800

    Top level FPU

---
+++ FPU/src/FPU.v
@@ -0,0 +1,44 @@
+`timescale 1ns / 1ps
+
+`include "../src/SqrtFPU.v"
+`include "../src/DivFPU.v"
+`include "../src/MulFPU.v"
+`include "../src/AddSubFPU.v"
+
+`define FPU_ADDSUB   2'b00
+`define FPU_MUL      2'b01
+`define FPU_DIV      2'b10
+`define FPU_SQRT     2'b11
+
+module FPU(
+    input [31:0] rs1,
+    input [31:0] rs2,
+    input [1:0] fpu_control,
+    input fpu_sel,
+    output reg [31:0] fpu_result
+    );
+
+    // temporary results for fpu operations
+    wire [31:0] AddSubResult;
+    wire [31:0] MulResult;
+    wire [31:0] DivResult;
+    wire [31:0] SqrtResult;
+
+    //module instantiation
+    AddSubFPU ASFPU_inst (.N1(rs1), .N2(rs2), .sel(fpu_sel), .result(AddSubResult));
+    MulFPU MulFPUinst(.N1(rs1),.N2(rs2),.result(MulResult));
+    DivFPU DivFPUinst(.N1(rs1), .N2(rs2), .result(DivResult));
+    SqrtFPU SqrtFPUinst(.A(rs1), .result(SqrtResult));
+
+    always @(*) begin
+        case(fpu_control) 
+            `FPU_ADDSUB: fpu_result = AddSubResult;
+            `FPU_MUL: fpu_result = MulResult;
+            `FPU_DIV: fpu_result = DivResult;
+            `FPU_SQRT: fpu_result = SqrtResult;
+            default:    
+                fpu_result = 32'b0;
+    endcase
+    end
+
+endmodule
---
+++ FPU/tb/FPU_tb.v
@@ -0,0 +1,219 @@
+`include "../src/FPU.v"
+
+module FPU_tb;
+reg [31:0] A,B;
+reg fpu_sel;
+reg [1:0] fpu_control;
+wire [31:0] result;
+real  value; 
+FPU dut (
+    .rs1(A),
+    .rs2(B),
+    .fpu_control(fpu_control),
+    .fpu_sel(fpu_sel),
+    .fpu_result(result)
+);
+
+initial  
+begin
+        $dumpfile("FPU.vcd");
+        $dumpvars(0,FPU_tb);
+//addition
+A = 32'b0_10000000_10011001100110011001100;  // 3.2
+B = 32'b0_10000001_00001100110011001100110;  // 4.2
+fpu_control = 2'b00;
+fpu_sel = 1'b0;
+#20
+A = 32'b0_01111110_01010001111010111000011;  // 0.66
+B = 32'b0_01111110_00000101000111101011100;  // 0.51
+fpu_control = 2'b00;
+fpu_sel = 1'b0;
+#20
+A = 32'b1_01111110_00000000000000000000000;  // -0.5
+B = 32'b1_10000001_10011001100110011001100;  // -6.4
+fpu_control = 2'b00;
+fpu_sel = 1'b0;
+#20
+A = 32'b1_01111110_00000000000000000000000;  // -0.5
+B = 32'b0_10000001_10011001100110011001100;  //  6.4
+fpu_control = 2'b00;
+fpu_sel = 1'b0;
+#20
+A = 32'h4034b4b5;
+B = 32'hbf70f0f1;
+fpu_control = 2'b00;
+fpu_sel = 1'b0;
+//subtraction
+#20
+A = 32'b0_10000000_10011001100110011001100;  // 3.2
+B = 32'b0_10000001_00001100110011001100110;  // 4.2
+fpu_control = 2'b00;
+fpu_sel = 1'b1;
+#20
+A = 32'b0_01111110_01010001111010111000011;  // 0.66
+B = 32'b0_01111110_00000101000111101011100;  // 0.51
+fpu_control = 2'b00;
+fpu_sel = 1'b1;
+#20
+A = 32'b1_01111110_00000000000000000000000;  // -0.5
+B = 32'b1_10000001_10011001100110011001100;  // -6.4
+fpu_control = 2'b00;
+fpu_sel = 1'b1;
+#20
+A = 32'b1_01111110_00000000000000000000000;  // -0.5
+B = 32'b0_10000001_10011001100110011001100;  //  6.4
+fpu_control = 2'b00;
+fpu_sel = 1'b1;
+#20
+A = 32'h4034b4b5;
+B = 32'hbf70f0f1;
+fpu_control = 2'b00;
+fpu_sel = 1'b1;
+//Multiplication
+#20
+A = 32'b0_10000000_10011001100110011001100;  // 3.2
+B = 32'b0_10000001_00001100110011001100110;  // 4.2
+fpu_control = 2'b01;
+#20
+A = 32'b0_01111110_01010001111010111000011;  // 0.66
+B = 32'b0_01111110_00000101000111101011100;  // 0.51
+fpu_control = 2'b01;
+#20
+A = 32'b1_01111110_00000000000000000000000;  // -0.5
+B = 32'b1_10000001_10011001100110011001100;  // -6.4
+fpu_control = 2'b01;
+#20
+A = 32'b1_01111110_00000000000000000000000;  // -0.5
+B = 32'b0_10000001_10011001100110011001100;  //  6.4
+fpu_control = 2'b01;
+#20
+A = 32'h4034b4b5;
+B = 32'hbf70f0f1;
+fpu_control = 2'b01;
+//Division
+#20
+B = 32'b0_10000000_10011001100110011001100;  // 3.2
+A = 32'b0_10000001_00001100110011001100110;  // 4.2
+fpu_control = 2'b10;
+#20
+A = 32'b0_01111110_01010001111010111000011;  // 0.66
+B = 32'b0_01111110_00000101000111101011100;  // 0.51
+fpu_control = 2'b10;
+#20
+B = 32'b1_01111110_00000000000000000000000;  // -0.5
+A = 32'b1_10000001_10011001100110011001100;  // -6.4
+fpu_control = 2'b10;
+#20
+B = 32'b1_01111110_00000000000000000000000;  // -0.5
+A = 32'b0_10000001_10011001100110011001100;  //  6.4
+fpu_control = 2'b10;
+#20
+A = 32'h4034b4b5;
+B = 32'hbf70f0f1;
+fpu_control = 2'b10;
+#20
+//Square Root
+A = 32'h41c80000;  // 25
+fpu_control = 2'b11;
+#20
+A = 32'h42040000;  // 33
+fpu_control = 2'b11;
+#20
+A = 32'h42aa0000;  // 85
+fpu_control = 2'b11;
+#20
+A = 32'h42b80000;  // 92
+fpu_control = 2'b11;
+end
+
+initial
+begin
+#15
+$display("===== Addition =====");
+value =(2**(result[30:23]-127))*($itor({1'b1,result[22:0]})/2**23)*((-1)**(result[31]));
+$display("Expected Value : %f Result : %f",3.2+4.2,value);
+#20
+value =(2**(result[30:23]-127))*($itor({1'b1,result[22:0]})/2**23)*((-1)**(result[31]));
+$display("Expected Value : %f Result : %f",0.66+0.51,value);
+#20
+value =(2**(result[30:23]-127))*($itor({1'b1,result[22:0]})/2**23)*((-1)**(result[31]));
+$display("Expected Value : %f Result : %f",-0.5+(-6.4),value);
+#20
+value =(2**(result[30:23]-127))*($itor({1'b1,result[22:0]})/2**23)*((-1)**(result[31]));
+$display("Expected Value : %f Result : %f",-0.5+6.4,value);
+#20
+value =(2**(result[30:23]-127))*($itor({1'b1,result[22:0]})/2**23)*((-1)**(result[31]));
+$display("Expected Value : %f Result : %f",2.82+(-0.94),value);
+
+
+#20
+$display("===== Subtraction =====");
+value =(2**(result[30:23]-127))*($itor({1'b1,result[22:0]})/2**23)*((-1)**(result[31]));
+$display("Expected Value : %f Result : %f",3.2-4.2,value);
+#20
+value =(2**(result[30:23]-127))*($itor({1'b1,result[22:0]})/2**23)*((-1)**(result[31]));
+$display("Expected Value : %f Result : %f",0.1-0.1,value);
+#20
+value =(2**(result[30:23]-127))*($itor({1'b1,result[22:0]})/2**23)*((-1)**(result[31]));
+$display("Expected Value : %f Result : %f",-0.5-(-6.4),value);
+#20
+value =(2**(result[30:23]-127))*($itor({1'b1,result[22:0]})/2**23)*((-1)**(result[31]));
+$display("Expected Value : %f Result : %f",-0.5-6.4,value);
+#20
+value =(2**(result[30:23]-127))*($itor({1'b1,result[22:0]})/2**23)*((-1)**(result[31]));
+$display("Expected Value : %f Result : %f",2.82-(-0.94),value);
+
+
+#20
+$display("===== Multiplication =====");
+value =(2**(result[30:23]-127))*($itor({1'b1,result[22:0]})/2**23)*((-1)**(result[31]));
+$display("Expected Value : %f Result : %f",3.2*4.2,value);
+#20
+value =(2**(result[30:23]-127))*($itor({1'b1,result[22:0]})/2**23)*((-1)**(result[31]));
+$display("Expected Value : %f Result : %f",0.1*0.1,value);
+#20
+value =(2**(result[30:23]-127))*($itor({1'b1,result[22:0]})/2**23)*((-1)**(result[31]));
+$display("Expected Value : %f Result : %f",-0.5*(-6.4),value);
+#20
+value =(2**(result[30:23]-127))*($itor({1'b1,result[22:0]})/2**23)*((-1)**(result[31]));
+$display("Expected Value : %f Result : %f",-0.5*6.4,value);
+#20
+value =(2**(result[30:23]-127))*($itor({1'b1,result[22:0]})/2**23)*((-1)**(result[31]));
+$display("Expected Value : %f Result : %f",2.82*(-0.94),value);
+
+
+#20
+$display("===== Division =====");
+value =(2**(result[30:23]-127))*($itor({1'b1,result[22:0]})/2**23)*((-1)**(result[31]));
+$display("Expected Value : %f Result : %f",4.2/3.2,value);
+#20
+value =(2**(result[30:23]-127))*($itor({1'b1,result[22:0]})/2**23)*((-1)**(result[31]));
+$display("Expected Value : %f Result : %f",0.66/0.51,value);
+#20
+value =(2**(result[30:23]-127))*($itor({1'b1,result[22:0]})/2**23)*((-1)**(result[31]));
+$display("Expected Value : %f Result : %f",(-6.4)/(-0.5),value);
+#20
+value =(2**(result[30:23]-127))*($itor({1'b1,result[22:0]})/2**23)*((-1)**(result[31]));
+$display("Expected Value : %f Result : %f",6.4/(-0.5),value);
+#20
+value =(2**(result[30:23]-127))*($itor({1'b1,result[22:0]})/2**23)*((-1)**(result[31]));
+$display("Expected Value : %f Result : %f",2.82/(-0.94),value);
+
+
+#20
+$display("===== Square root =====");
+value =(2**(result[30:23]-127))*($itor({1'b1,result[22:0]})/2**23)*((-1)**(result[31]));
+$display("Expected Value : %f Result : %f",5.0,value);
+#20
+value =(2**(result[30:23]-127))*($itor({1'b1,result[22:0]})/2**23)*((-1)**(result[31]));
+$display("Expected Value : %f Result : %f",5.744562646538029,value);
+#20
+value =(2**(result[30:23]-127))*($itor({1'b1,result[22:0]})/2**23)*((-1)**(result[31]));
+$display("Expected Value : %f Result : %f",9.219544457292887,value);
+#20
+value =(2**(result[30:23]-127))*($itor({1'b1,result[22:0]})/2**23)*((-1)**(result[31]));
+$display("Expected Value : %f Result : %f",9.591663046625438,value);
+$finish;
+end
+
+endmodule
