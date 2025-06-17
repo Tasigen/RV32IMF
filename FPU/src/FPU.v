a072b8cd2cbaee3f5a3c3c08341ceceead17079f
Tasigen <hibiscus_ts@yahoo.com>
2025-06-17 22:39:01 +0800

    FPU with Sign Injection

--- FPU/src/FPU.v
+++ FPU/src/FPU.v
@@ -2,15 +2,16 @@
 
 
 
-`define FPU_ADDSUB   2'b00
-`define FPU_MUL      2'b01
-`define FPU_DIV      2'b10
-`define FPU_SQRT     2'b11
+`define FPU_ADDSUB   3'b000, 3'b001
+`define FPU_MUL      3'b010
+`define FPU_DIV      3'b011
+`define SIGN_INJECTION  3'b100
 
 module FPU(
     input [31:0] rs1,
     input [31:0] rs2,
-    input [1:0] fpu_control,
+    input [2:0] fpu_control,
+    input [2:0] funct3,
     input fpu_sel,
     output reg [31:0] fpu_result
     );
@@ -20,19 +21,26 @@ module FPU(
     wire [31:0] MulResult;
     wire [31:0] DivResult;
     wire [31:0] SqrtResult;
+    wire sign_temp;
+
+    assign sign_temp = rs1[31] ^ rs2[31];
 
     //module instantiation
     AddSubFPU ASFPU_inst (.N1(rs1), .N2(rs2), .sel(fpu_sel), .result(AddSubResult));
     MulFPU MulFPUinst(.N1(rs1),.N2(rs2),.result(MulResult));
     DivFPU_Flowchart DivFPUinst(.N1(rs1), .N2(rs2), .result(DivResult));
-    //SqrtFPU SqrtFPUinst(.A(rs1), .result(SqrtResult));
 
     always @(*) begin
         case(fpu_control) 
             `FPU_ADDSUB: fpu_result = AddSubResult;
             `FPU_MUL: fpu_result = MulResult;
             `FPU_DIV: fpu_result = DivResult;
-            `FPU_SQRT: fpu_result = SqrtResult;
+            `SIGN_INJECTION: begin
+                case(funct3)
+                    3'b000:fpu_result = {rs2[31],rs1[30:0]} ;
+                    3'b010:fpu_result = {sign_temp,rs1[30:0]};
+                endcase
+            end
             default:    
                 fpu_result = 32'b0;
     endcase
--- FPU/tb/FPU_tb.v
+++ FPU/tb/FPU_tb.v
@@ -6,13 +6,15 @@
 module FPU_tb;
 reg [31:0] A,B;
 reg fpu_sel;
-reg [1:0] fpu_control;
+reg [2:0] fpu_control;
+reg [2:0] funct3;
 wire [31:0] result;
 real  value; 
 FPU dut (
     .rs1(A),
     .rs2(B),
     .fpu_control(fpu_control),
+    .funct3(funct3),
     .fpu_sel(fpu_sel),
     .fpu_result(result)
 );
@@ -24,109 +26,109 @@ begin
 //addition
 A = 32'b0_10000000_10011001100110011001100;  // 3.2
 B = 32'b0_10000001_00001100110011001100110;  // 4.2
-fpu_control = 2'b00;
+fpu_control = 3'b000;
 fpu_sel = 1'b0;
 #20
 A = 32'b0_01111110_01010001111010111000011;  // 0.66
 B = 32'b0_01111110_00000101000111101011100;  // 0.51
-fpu_control = 2'b00;
+fpu_control = 3'b000;
 fpu_sel = 1'b0;
 #20
 A = 32'b1_01111110_00000000000000000000000;  // -0.5
 B = 32'b1_10000001_10011001100110011001100;  // -6.4
-fpu_control = 2'b00;
+fpu_control = 3'b000;
 fpu_sel = 1'b0;
 #20
 A = 32'b1_01111110_00000000000000000000000;  // -0.5
 B = 32'b0_10000001_10011001100110011001100;  //  6.4
-fpu_control = 2'b00;
+fpu_control = 3'b000;
 fpu_sel = 1'b0;
 #20
 A = 32'h4034b4b5;
 B = 32'hbf70f0f1;
-fpu_control = 2'b00;
+fpu_control = 3'b000;
 fpu_sel = 1'b0;
 //subtraction
 #20
 A = 32'b0_10000000_10011001100110011001100;  // 3.2
 B = 32'b0_10000001_00001100110011001100110;  // 4.2
-fpu_control = 2'b00;
+fpu_control = 3'b001;
 fpu_sel = 1'b1;
 #20
 A = 32'b0_01111110_01010001111010111000011;  // 0.66
 B = 32'b0_01111110_00000101000111101011100;  // 0.51
-fpu_control = 2'b00;
+fpu_control = 3'b001;
 fpu_sel = 1'b1;
 #20
 A = 32'b1_01111110_00000000000000000000000;  // -0.5
 B = 32'b1_10000001_10011001100110011001100;  // -6.4
-fpu_control = 2'b00;
+fpu_control = 3'b001;
 fpu_sel = 1'b1;
 #20
 A = 32'b1_01111110_00000000000000000000000;  // -0.5
 B = 32'b0_10000001_10011001100110011001100;  //  6.4
-fpu_control = 2'b00;
+fpu_control = 3'b001;
 fpu_sel = 1'b1;
 #20
 A = 32'h4034b4b5;
 B = 32'hbf70f0f1;
-fpu_control = 2'b00;
+fpu_control = 3'b001;
 fpu_sel = 1'b1;
 //Multiplication
 #20
 A = 32'b0_10000000_10011001100110011001100;  // 3.2
 B = 32'b0_10000001_00001100110011001100110;  // 4.2
-fpu_control = 2'b01;
+fpu_control = 3'b010;
 #20
 A = 32'b0_01111110_01010001111010111000011;  // 0.66
 B = 32'b0_01111110_00000101000111101011100;  // 0.51
-fpu_control = 2'b01;
+fpu_control = 3'b010;
 #20
 A = 32'b1_01111110_00000000000000000000000;  // -0.5
 B = 32'b1_10000001_10011001100110011001100;  // -6.4
-fpu_control = 2'b01;
+fpu_control = 3'b010;
 #20
 A = 32'b1_01111110_00000000000000000000000;  // -0.5
 B = 32'b0_10000001_10011001100110011001100;  //  6.4
-fpu_control = 2'b01;
+fpu_control = 3'b010;
 #20
 A = 32'h4034b4b5;
 B = 32'hbf70f0f1;
-fpu_control = 2'b01;
+fpu_control = 3'b010;
 //Division
 #20
 B = 32'b0_10000000_10011001100110011001100;  // 3.2
 A = 32'b0_10000001_00001100110011001100110;  // 4.2
-fpu_control = 2'b10;
+fpu_control = 3'b011;
 #20
 A = 32'b0_01111110_01010001111010111000011;  // 0.66
 B = 32'b0_01111110_00000101000111101011100;  // 0.51
-fpu_control = 2'b10;
+fpu_control = 3'b011;
 #20
 B = 32'b1_01111110_00000000000000000000000;  // -0.5
 A = 32'b1_10000001_10011001100110011001100;  // -6.4
-fpu_control = 2'b10;
+fpu_control = 3'b011;
 #20
-B = 32'b1_01111110_00000000000000000000000;  // -0.5
-A = 32'b0_10000001_10011001100110011001100;  //  6.4
-fpu_control = 2'b10;
+A = 32'b1_01111110_00000000000000000000000;  // -0.5
+B = 32'b0_10000001_10011001100110011001100;  //  6.4
+fpu_control = 3'b011;
 #20
 A = 32'h4034b4b5;
 B = 32'hbf70f0f1;
-fpu_control = 2'b10;
+fpu_control = 3'b011;
 #20
-//Square Root
-A = 32'h41c80000;  // 25
-fpu_control = 2'b11;
+
+//sign injections
+A = 32'b1_01111110_00000000000000000000000;  // -0.5
+B = 32'b0_10000001_10011001100110011001100;  //  6.4
+fpu_control = 3'b100;
+funct3 = 3'b000;
 #20
-A = 32'h42040000;  // 33
-fpu_control = 2'b11;
-#20
-A = 32'h42aa0000;  // 85
-fpu_control = 2'b11;
-#20
-A = 32'h42b80000;  // 92
-fpu_control = 2'b11;
+A = 32'b1_01111110_00000000000000000000000;  // -0.5
+B = 32'b0_10000001_10011001100110011001100;  //  6.4
+fpu_control = 3'b100;
+funct3 = 3'b010;
+
 end
 
 initial
@@ -197,25 +199,20 @@ value =(2**(result[30:23]-127))*($itor({1'b1,result[22:0]})/2**23)*((-1)**(resul
 $display("Expected Value : %f Result : %f Hex Values : %h",(-6.4)/(-0.5),value, result);
 #20
 value =(2**(result[30:23]-127))*($itor({1'b1,result[22:0]})/2**23)*((-1)**(result[31]));
-$display("Expected Value : %f Result : %f Hex Values : %h",6.4/(-0.5),value, result);
+$display("Expected Value : %f Result : %f Hex Values : %h",(-0.5)/6.4,value, result);
 #20
 value =(2**(result[30:23]-127))*($itor({1'b1,result[22:0]})/2**23)*((-1)**(result[31]));
 $display("Expected Value : %f Result : %f Hex Values : %h",2.82/(-0.94),value, result);
 
+#20
+$display("===== Sign Injection =====");
+value =(2**(result[30:23]-127))*($itor({1'b1,result[22:0]})/2**23)*((-1)**(result[31]));
+$display("Expected Value : %f Result : %f Hex Values : %h",0.5,value, result);
+#20
+value =(2**(result[30:23]-127))*($itor({1'b1,result[22:0]})/2**23)*((-1)**(result[31]));
+$display("Expected Value : %f Result : %f Hex Values : %h",-0.5,value, result);
 
-/*#20
-$display("===== Square root =====");
-value =(2**(result[30:23]-127))*($itor({1'b1,result[22:0]})/2**23)*((-1)**(result[31]));
-$display("Expected Value : %f Result : %f",5.0,value);
-#20
-value =(2**(result[30:23]-127))*($itor({1'b1,result[22:0]})/2**23)*((-1)**(result[31]));
-$display("Expected Value : %f Result : %f",5.744562646538029,value);
-#20
-value =(2**(result[30:23]-127))*($itor({1'b1,result[22:0]})/2**23)*((-1)**(result[31]));
-$display("Expected Value : %f Result : %f",9.219544457292887,value);
-#20
-value =(2**(result[30:23]-127))*($itor({1'b1,result[22:0]})/2**23)*((-1)**(result[31]));
-$display("Expected Value : %f Result : %f",9.591663046625438,value);*/
+
 $finish;
 end
 
